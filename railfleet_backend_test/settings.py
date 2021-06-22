c7d3f031d2302cc574ab924d2594806fadf37761
Flying Sausages <flying_sausages@protonmail.com>
2021-06-22 18:19:49 +0200

    feat: add celery

--- railfleet_backend_test/settings.py
+++ railfleet_backend_test/settings.py
@@ -134,3 +134,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Europe/Brussels'
