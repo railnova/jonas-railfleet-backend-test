f205e5be2376b5b71bd3f3894486b883582a0f93
Flying Sausages <flying_sausages@protonmail.com>
2021-06-22 16:18:16 +0200

    test: add little helper to see that deno receives sth

--- counters_server.js
+++ counters_server.js
@@ -3,8 +3,11 @@ import { serve } from "https://deno.land/std/http/mod.ts";
 const server = serve({ port: 8888 });
 console.log("Counter server is up!");
 console.log("Waiting on new request...");
+console.log("");
 for await (const req of server) {
   await req.respond({
     status: 200,
   });
+  console.log("Received a thing")
+  // console.log(Deno.readAll(req.body))
 }
